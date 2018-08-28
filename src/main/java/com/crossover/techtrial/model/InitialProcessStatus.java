dd9cf27159bebc4a9dca0130421a4c7fb65c47c4
KareemSayed <eng.kareemsayed90@gmail.com>
2018-08-28 13:25:37 +0200

    My submission

---
+++ src/main/java/com/crossover/techtrial/model/InitialProcessStatus.java
@@ -0,0 +1,12 @@
+package com.crossover.techtrial.model;
+
+/**
+ * Available statuses for initial process.
+ *
+ * @author KareemSayed
+ */
+public enum InitialProcessStatus {
+    STARTED,
+    FINISHED,
+    FAILED
+}
